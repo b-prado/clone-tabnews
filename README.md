f7bb6f468932a04fbb486f63657c7384ff4f0424
b-prado <71569202+b-prado@users.noreply.github.com>
2026-03-09 07:03:22 +0000

    Added scripts `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementation of tabnews.com.br for the course curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Hello, how was your day?</h1>
+  return <h1>Hello, how was your day?</h1>;
 }
 
 export default Home;
